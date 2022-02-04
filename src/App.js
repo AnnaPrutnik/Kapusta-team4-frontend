80336d8a180595007b8d9125ecef51e3cf11de8b
Anna Prutnik <prutnik.anna@gmail.com>
2022-02-04 22:28:42 +0200

    fix eslint, add scss, modern-normalize, container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello Team!</h1>
+    <div className="App">
+      <section className="container">
+        <h1>Hello Team!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+          placeat laudantium illo! Deserunt adipisci quaerat nulla praesentium
+          ipsum eum exercitationem fuga libero possimus, laudantium illum
+          doloremque dolore expedita sunt, corrupti sed. Cum nesciunt animi
+          possimus perspiciatis neque distinctio rem hic qui commodi laborum
+          illum, sed ducimus, debitis iusto quo eveniet!
+        </p>
+      </section>
     </div>
   );
 }
