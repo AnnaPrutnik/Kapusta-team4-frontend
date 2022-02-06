502f7e8b68fc76e9c32bb04db836d0c58564e69e
Anna Prutnik <prutnik.anna@gmail.com>
2022-02-06 12:44:37 +0200

    some changes to login form, kill eslint, add base style for list, link, form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-
-import Home from "./components/Home/Home";
-import LoginForm from './components/LoginForm/LoginForm'
-
+import Home from './components/Home/Home';
+import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <section className="container">
-          <Home/>
-       <LoginForm />
-        <h1>Hello Team!</h1>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          placeat laudantium illo! Deserunt adipisci quaerat nulla praesentium
-          ipsum eum exercitationem fuga libero possimus, laudantium illum
-          doloremque dolore expedita sunt, corrupti sed. Cum nesciunt animi
-          possimus perspiciatis neque distinctio rem hic qui commodi laborum
-          illum, sed ducimus, debitis iusto quo eveniet!
-
-        </p> 
- 
-
-      </section>
-
+      <div className="wrapper">
+        <Home />
+        <LoginForm />
+        <section className="container"></section>
+      </div>
     </div>
   );
 }
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,66 +1,56 @@
-import s from './login.module.css'
+import s from './Login.module.scss';
 
 const LoginForm = () => {
-    return (
-        <div className={s.formRegister}>
-            <p className={s.promtText}>Вы можете авторизоваться с помощью Google Account:</p>
-            <div className={s.animationGoogle}>
-                <a
-                    href="http://localhost:3000/auth/google"
-                    className={s.btnGoogle}>Google
-                </a>
-            </div>
-            <p className={s.promtText}>
-                Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
-            </p>
-            <form action="">
-                <div>
-                    <lable className={s.formLabel}>
-                        <p className={s.labelText}>
-                            <span></span>
-                            Электронная почта:
-                        </p>
-                        <input
-                            className={s.formInput}
-                            type="email"
-                            name="email"
-                            // value={email}
-                            placeholder="your@email.com"
-                            pattern="[A-Za-zА-Яа-яЁёЄєЇї0-9._%+-]+@[A-Za-zА-Яа-яЁёЄєЇї0-9.-]+\.[A-Za-zА-Яа-яЁёЄєЇї]{2,4}$"
-                            title="Email может, сoстоять из букв цифр и обязательного символа '@'"
-                            required
-                        />
-                    </lable>
-                </div>
-                <div>
-                    <lable className={s.formLabel}>
-                        <p className={s.labelText}>
-                            <span></span>
-                            Пароль:
-                        </p>
-                        <input
-                            className={s.formInput}
-                            type="password"
-                            name="password"
-                            // value={password}
-                            placeholder="Пароль"
-                            pattern="[0-9A-Za-zА-Яа-яЁёЄєЇї!@#$%^&*]{6,}"
-                            title="Пароль может, сoстоять не меньше чем из шести букв цифр и символов '!@#$%^&*'"
-                            required
-                        />
-                    </lable>
-                </div>
-                <div className={s.containerButton}>
-                    <button type="submit" className={s.button}>
-                        ВОЙТИ
-                    </button>
-                    <button type="button" className={s.button}>
-                        РЕГИСТРАЦИЯ
-                    </button>
-                </div>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className={s.register}>
+      <p className={s.text_align}>
+        Вы можете авторизоваться с помощью Google Account:
+      </p>
+      <a href="http://localhost:3000/auth/google" className={s.google}>
+        Google
+      </a>
+      <p className={s.text}>
+        Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
+      </p>
+      <form action="">
+        <label className={s.label}>
+          <p className={s.sign}>Электронная почта:</p>
+          <input
+            className={s.input}
+            type="email"
+            name="email"
+            // value={email}
+            placeholder="your@email.com"
+            pattern="[A-Za-zА-Яа-яЁёЄєЇї0-9._%+-]+@[A-Za-zА-Яа-яЁёЄєЇї0-9.-]+\.[A-Za-zА-Яа-яЁёЄєЇї]{2,4}$"
+            title="Email может, сoстоять из букв цифр и обязательного символа '@'"
+            required
+          />
+        </label>
 
-export default LoginForm
+        <label className={s.last_label}>
+          <p className={s.sign}>Пароль:</p>
+          <input
+            className={s.input}
+            type="password"
+            name="password"
+            // value={password}
+            placeholder="Пароль"
+            pattern="[0-9A-Za-zА-Яа-яЁёЄєЇї!@#$%^&*]{6,}"
+            title="Пароль может, сoстоять не меньше чем из шести букв цифр и символов '!@#$%^&*'"
+            required
+          />
+        </label>
+        <div className={s.wrap}>
+          <button type="submit" className={s.button}>
+            войти
+          </button>
+          <button type="button" className={s.button}>
+            регистрация
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
