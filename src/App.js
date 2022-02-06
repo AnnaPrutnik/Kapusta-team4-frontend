f99250ad119afb986694f3ce6f9f2ef70388d91b
Anna Prutnik <prutnik.anna@gmail.com>
2022-02-06 22:04:46 +0200

    add changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import ExpenseAndIncome from "../src/components/ExpenseAndIncome/ExpenseAndIncome";
-import Home from './components/Home/Home';
-import LoginForm from './components/LoginForm/LoginForm';
+import ExpenseAndIncome from '../src/components/ExpenseAndIncome/ExpenseAndIncome'
+import Home from './components/Home/Home'
+import LoginForm from './components/LoginForm/LoginForm'
 
-import ReportView from './views/ReportView/ReportView';
+import ReportView from './views/ReportView/ReportView'
 
 function App() {
   return (
     <div className="App">
-      
       <div className="wrapper">
         <Home />
         <LoginForm />
 
         <section className="container">
-          <ExpenseAndIncome/>
+          <ExpenseAndIncome />
+        </section>
+        <section className="container">
           <ReportView />
-          
         </section>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
