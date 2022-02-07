6eab7d8df6ec0fb80556bd584b034f675b3e592b
Anna Prutnik <prutnik.anna@gmail.com>
2022-02-07 15:13:26 +0200

    add chart

--- src/views/ReportView/ReportView.js
+++ src/views/ReportView/ReportView.js
@@ -2,7 +2,50 @@ import React from 'react'
 import IncomeOutcomeMenu from '../../components/Report/ReportMenu/ReportMenu'
 import ReportCategoryList from '../../components/Report/ReportCategoryList/ReportCategoryList'
 // import BalanceBar from '../../components/BalanceBar/BalanceBar';
-// import Chart from '../../components/Chart/Chart';
+import Chart from '../../components/Chart/Chart'
+
+const data = [
+  {
+    name: 'Свинина',
+    quantity: 5000,
+  },
+  {
+    name: 'Говядина',
+    quantity: 4500,
+  },
+  {
+    name: 'Курица',
+    quantity: 3200,
+  },
+  {
+    name: 'Рыба',
+    quantity: 2100,
+  },
+  {
+    name: 'Панини',
+    quantity: 1800,
+  },
+  {
+    name: 'Кофе',
+    quantity: 1700,
+  },
+  {
+    name: 'Спагетти',
+    quantity: 1500,
+  },
+  {
+    name: 'Шоколад',
+    quantity: 800,
+  },
+  {
+    name: 'Маслины',
+    quantity: 500,
+  },
+  {
+    name: 'Зелень',
+    quantity: 300,
+  },
+]
 
 const ReportView = () => {
   return (
@@ -10,7 +53,7 @@ const ReportView = () => {
       {/* <BalanceBar /> */}
       <IncomeOutcomeMenu />
       <ReportCategoryList />
-      {/* <Chart/> */}
+      <Chart data={data} />
     </section>
   )
 }
