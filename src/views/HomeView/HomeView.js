405a3d6682e051865f1f4731de1ab0c3227bb4cc
Anna Prutnik <prutnik.anna@gmail.com>
2022-02-09 19:30:03 +0200

    fix home

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -3,17 +3,8 @@ import Form from '../../components/Form/Form'
 import { getExpenseCategory } from '../../services/categoryApi/categoryApi'
 
 const HomeView = () => {
-  const handleClick = async () => {
-    console.log('click')
-    const categories = await getExpenseCategory()
-    console.log(categories)
-  }
   return (
     <section className="home_section">
-      <HomeContent />
-      <button onClick={handleClick} type="button">
-        Test axios
-      </button>
       <Form />
     </section>
   )
